5bda9df69e2b1a0b775e6fa9851f6de395622d07
sam wilson <sam.wilson@remarkable.net>
2025-01-31 08:38:11 +0000

    aaaa

--- content/themes.js
+++ content/themes.js
@@ -1,21 +1,32 @@
 themeRandomizer = () => {
-    const theme = ["gold", "pastel", "alcest", "griffith", "guts", "stairs"]
+    const plainThemes = ["pastel", "griffith", "guts", "stairs"]
+    const otherTheme = ["aries", "cow"]
+
+    const theme = plainThemes;
+
+    const now = new Date()
+    const check = new Date();
+    check.setHours(21,0, 0, 0);
+    if(now > check) {
+        theme.push(otherTheme);
+    }
+
     const random = theme[Math.floor(Math.random() * theme.length)]
     return random
+}
+    
+setTheme = () => {
+    const selector = document.getElementsByClassName("image")[0]
+    savedTheme = localStorage.getItem('theme')
+    if(savedTheme) {
+        document.documentElement.className = savedTheme
     }
     
-    setTheme = () => {
-        const selector = document.getElementsByClassName("image")[0]
-        savedTheme = localStorage.getItem('theme')
-        if(savedTheme) {
-            document.documentElement.className = savedTheme
-        }
-        
-        selector.addEventListener("click", () => {
-            theme = themeRandomizer()
-            document.documentElement.className = theme
-            localStorage.setItem('theme', theme)
-        })
-    }
+    selector.addEventListener("click", () => {
+        theme = themeRandomizer()
+        document.documentElement.className = theme
+        localStorage.setItem('theme', theme)
+    })
+}
     
-    setTheme()
+setTheme()
